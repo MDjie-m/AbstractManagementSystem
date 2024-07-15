7c76f1dbc45016874f908639b5d91e61ace3b5b9
郭靖 <17320070087@163.com>
2024-07-15 20:22:25 +0800

    报告导出后端文件

---
+++ ruoyi-admin/src/main/java/com/ruoyi/reportSys/mapper/ReportManagerMapper.java
@@ -0,0 +1,15 @@
+package com.ruoyi.reportSys.mapper;
+
+import com.ruoyi.reportSys.domain.HistoricalInspectionReport;
+
+import java.util.List;
+
+/**
+ * 
+ * @author GG
+ * @date 2024-07-15
+ */
+public interface ReportManagerMapper
+{
+
+}
---
+++ ruoyi-admin/src/main/java/com/ruoyi/reportSys/service/ReportManagerService.java
@@ -0,0 +1,15 @@
+package com.ruoyi.reportSys.service;
+
+import com.ruoyi.reportSys.domain.HistoricalInspectionReport;
+
+import java.util.List;
+
+/**
+ * 
+ * @author GG
+ * @date 2024-07-15
+ */
+public interface ReportManagerService
+{
+
+}
