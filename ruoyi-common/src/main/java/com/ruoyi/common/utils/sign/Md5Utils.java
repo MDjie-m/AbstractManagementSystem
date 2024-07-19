e46b9c061da7fafa9b78941f37cf0486b839b4d1
徐祥 <642578464@qq.com>
2024-07-19 14:24:56 +0800

    1. 后台用户登录新增错误次数过多暂时封ip,次数和时间可以在yml里配置,防止恶意登录请求
    2. FileUploadUtils里文件上传新增秒传功能,重复的文件不会重复上传了,文件的md5暂时存在redis里,后面可以存数据库,这样迁移更方便

--- ruoyi-common/src/main/java/com/ruoyi/common/utils/sign/Md5Utils.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/utils/sign/Md5Utils.java
@@ -1,18 +1,31 @@
 package com.ruoyi.common.utils.sign;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Md5加密方法
- * 
+ *
  * @author ruoyi
  */
 public class Md5Utils
 {
     private static final Logger log = LoggerFactory.getLogger(Md5Utils.class);
+    private static final ThreadLocal<MessageDigest> md5DigestThreadLocal = ThreadLocal.withInitial(() -> {
+        try {
+            return MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 algorithm not found", e);
+        }
+    });
 
     private static byte[] md5(String s)
     {
@@ -63,5 +76,75 @@ public class Md5Utils
             log.error("not supported charset...{}", e);
             return s;
         }
+    }
+
+    /**
+     * 计算文件的md5
+     * @param file 文件
+     * @return
+     */
+    public static String getMd5(MultipartFile file) {
+        try {
+            long fileSize = file.getSize();
+            // 10MB作为分界点
+            if (fileSize < 10 * 1024 * 1024) {
+                return getMd5ForSmallFile(file);
+            } else {
+                return getMd5ForLargeFile(file);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * 计算小文件的md5
+     *
+     * @param file
+     * @return
+     */
+    public static String getMd5ForSmallFile(MultipartFile file) {
+        try {
+            // 获取文件的byte信息
+            byte[] uploadBytes = file.getBytes();
+            // 拿到一个MD5转换器
+            MessageDigest md5 = md5DigestThreadLocal.get();
+            byte[] digest = md5.digest(uploadBytes);
+            // 转换为16进制
+            String md5Hex = new BigInteger(1, digest).toString(16);
+            // 确保MD5值始终是32位
+            while (md5Hex.length() < 32) {
+                md5Hex = "0" + md5Hex;
+            }
+            return md5Hex;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return null;
+    }
+
+    /**
+     * 计算大文件的md5
+     *
+     * @param file
+     * @return
+     */
+    private static String getMd5ForLargeFile(MultipartFile file) throws IOException {
+        MessageDigest md = md5DigestThreadLocal.get();
+        try (InputStream is = file.getInputStream()) {
+            byte[] buffer = new byte[81920];
+            int read;
+            while ((read = is.read(buffer)) != -1) {
+                md.update(buffer, 0, read);
+            }
+        }
+
+        byte[] digest = md.digest();
+        StringBuilder sb = new StringBuilder();
+        for (byte b : digest) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
     }
 }
