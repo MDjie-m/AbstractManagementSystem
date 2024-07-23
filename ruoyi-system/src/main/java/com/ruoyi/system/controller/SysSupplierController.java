66c921e4e9470ea390442e85f0a309ab186c2f34
YichengTang <724739464@qq.com>
2024-07-23 09:56:20 +0800

    小修改，给上传文件的参数后加了个注解

--- ruoyi-system/src/main/java/com/ruoyi/system/controller/SysSupplierController.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/controller/SysSupplierController.java
@@ -10,14 +10,7 @@ import com.alibaba.excel.write.style.column.LongestMatchColumnWidthStyleStrategy
 import com.ruoyi.system.easyexcel.SupplierListener;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.core.controller.BaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
@@ -61,7 +54,7 @@ public class SysSupplierController extends BaseController
     @Log(title = "供应商管理", businessType = BusinessType.IMPORT)
     @PreAuthorize("@ss.hasPermi('system:supplier:import')")
     @PostMapping("/importData")
-    public AjaxResult importData(MultipartFile file) throws Exception
+    public AjaxResult importData(@RequestParam("file") MultipartFile file) throws Exception
     {
         // 可以加一个boolean判断要不要覆盖现在是默认重复就覆盖
         EasyExcel
