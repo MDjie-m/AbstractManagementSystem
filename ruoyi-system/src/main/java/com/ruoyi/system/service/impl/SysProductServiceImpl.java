5c8a9635fbbc4dcbe7829b745d485fa96b1526ed
l7AliceForYou <1575928011@qq.com>
2024-07-25 14:16:35 +0800

    feat: 新增产品的导入

--- ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysProductServiceImpl.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/impl/SysProductServiceImpl.java
@@ -1,6 +1,11 @@
 package com.ruoyi.system.service.impl;
 
 import java.util.List;
+import com.ruoyi.common.exception.ServiceException;
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.common.utils.uuid.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.ruoyi.system.mapper.SysProductMapper;
@@ -14,8 +19,9 @@ import com.ruoyi.system.service.ISysProductService;
  * @date 2024-07-23
  */
 @Service
-public class SysProductServiceImpl implements ISysProductService 
-{
+public class SysProductServiceImpl implements ISysProductService {
+    private static final Logger log = LoggerFactory.getLogger(SysProductServiceImpl.class);
+
     @Autowired
     private SysProductMapper sysProductMapper;
 
@@ -89,5 +95,54 @@ public class SysProductServiceImpl implements ISysProductService
     public int deleteSysProductByProductId(String productId)
     {
         return sysProductMapper.deleteSysProductByProductId(productId);
+    }
+
+    /**
+     * 导入产品数据
+     *
+     * @param productList     产品数据列表
+     * @param isUpdateSupport 是否更新支持，如果已存在，则进行更新数据
+     * @return
+     */
+    @Override
+    public String importProduct(List<SysProduct> productList, Boolean isUpdateSupport) {
+        if (StringUtils.isNull(productList) || productList.size() == 0) {
+            throw new ServiceException("导入产品数据不能为空！");
+        }
+        int successNum = 0;
+        int failureNum = 0;
+        StringBuilder successMsg = new StringBuilder();
+        StringBuilder failureMsg = new StringBuilder();
+        for (SysProduct sysProduct : productList) {
+            try {
+                // 验证
+                List<SysProduct> list = sysProductMapper.selectSysProductList(sysProduct);
+                if (list.isEmpty()) {
+                    sysProduct.setProductId(UUID.randomUUID().toString());
+                    sysProductMapper.insertSysProduct(sysProduct);
+                    successNum++;
+                    successMsg.append("<br/>" + successNum + "、产品 " + sysProduct.getProductName() + " 导入成功");
+                } else if (isUpdateSupport) {
+                    sysProductMapper.updateSysProduct(sysProduct);
+                    successNum++;
+                    successMsg.append("<br/>" + successNum + "、产品 " + sysProduct.getProductName() + " 更新成功");
+                } else {
+                    failureNum++;
+                    failureMsg.append("<br/>" + failureNum + "、产品 " + sysProduct.getProductName() + " 已存在");
+                }
+            } catch (Exception e) {
+                failureNum++;
+                String msg = "<br/>" + failureNum + "、产品 " + sysProduct.getProductName() + " 导入失败：";
+                failureMsg.append(msg + e.getMessage());
+                log.error(msg, e);
+            }
+        }
+        if (failureNum > 0) {
+            failureMsg.insert(0, "很抱歉，导入失败！共 " + failureNum + " 条数据格式不正确，错误如下：");
+            throw new ServiceException(failureMsg.toString());
+        } else {
+            successMsg.insert(0, "恭喜您，数据已全部导入成功！共 " + successNum + " 条，数据如下：");
+        }
+        return successMsg.toString();
     }
 }
