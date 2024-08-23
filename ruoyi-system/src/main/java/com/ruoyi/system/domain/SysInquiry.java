0fe90d41031323247884fba1c6c377b530e66030
YichengTang <724739464@qq.com>
2024-08-23 11:25:42 +0800

    修改询价id类型和询价员id类型

--- ruoyi-system/src/main/java/com/ruoyi/system/domain/SysInquiry.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/domain/SysInquiry.java
@@ -19,10 +19,10 @@ public class SysInquiry extends BaseEntity
     private static final long serialVersionUID = 1L;
 
     /** 询价id */
-    private String inquiryId;
+    private Long inquiryId;
 
     /** 采购员id */
-    private String buyerId;
+    private Long buyerId;
 
     /** 产品id */
     private String productId;
@@ -47,21 +47,21 @@ public class SysInquiry extends BaseEntity
     /** 0-未删除，1-已删除 */
     private Long delFlg;
 
-    public void setInquiryId(String inquiryId) 
+    public void setInquiryId(Long inquiryId)
     {
         this.inquiryId = inquiryId;
     }
 
-    public String getInquiryId() 
+    public Long getInquiryId()
     {
         return inquiryId;
     }
-    public void setBuyerId(String buyerId) 
+    public void setBuyerId(Long buyerId)
     {
         this.buyerId = buyerId;
     }
 
-    public String getBuyerId() 
+    public Long getBuyerId()
     {
         return buyerId;
     }
