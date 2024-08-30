4a17f2437ca583cc883b8b9422f67fd7546f3294
GalaxyLordLeo <joker_52it@163.com>
2024-08-30 02:58:59 +0000

    update ruoyi-ui/src/utils/ruoyi.js. 导出功能添加支持日期类型的参数
    Signed-off-by: GalaxyLordLeo <joker_52it@163.com>

--- ruoyi-ui/src/utils/ruoyi.js
+++ ruoyi-ui/src/utils/ruoyi.js
@@ -212,11 +212,19 @@ export function tansParams(params) {
     var part = encodeURIComponent(propName) + "=";
     if (value !== null && value !== "" && typeof (value) !== "undefined") {
       if (typeof value === 'object') {
-        for (const key of Object.keys(value)) {
-          if (value[key] !== null && value[key] !== "" && typeof (value[key]) !== 'undefined') {
-            let params = propName + '[' + key + ']';
-            var subPart = encodeURIComponent(params) + "=";
-            result += subPart + encodeURIComponent(value[key]) + "&";
+        if (value instanceof Date) {
+          result += part + encodeURIComponent(value) + "&";
+        } else {
+          for (const key of Object.keys(value)) {
+            if (
+              value[key] !== null &&
+              value[key] !== "" &&
+              typeof value[key] !== "undefined"
+            ) {
+              let params = propName + "[" + key + "]";
+              var subPart = encodeURIComponent(params) + "=";
+              result += subPart + encodeURIComponent(value[key]) + "&";
+            }
           }
         }
       } else {
