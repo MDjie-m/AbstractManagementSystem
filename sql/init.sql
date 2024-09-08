67a41c694fb9460e93d56bb767d39d5ed94e176d
eric <zeng3750@foxmail.com>
2024-09-08 15:56:43 +0800

    增加商品管理

--- sql/init.sql
+++ sql/init.sql
@@ -98,18 +98,24 @@ create table  t_store_desk
     remark        nvarchar(500)                          null comment '备注'
 )
 comment '球桌';
+drop table  if exists t_order;
 create table  t_order
 (
     order_id      bigint                                not null comment '订单id' primary key,
     order_no varchar(60) not null  comment '订单编码',
     order_type int not null  comment '类型：1=球桌费用，2=会员充值，3=商品购买,4=陪练费用，5=教学费用',
-    total_amount decimal(10,2) null  comment '总费用',
+    total_amount_due decimal(10,2)  not null comment '应付总金额 ',
+    total_discount_amount decimal(10,2) null  comment '折扣金额',
+    total_amount decimal(10,2) null  comment '实际支付金额',
+    pay_type int null  comment '支付方式：0=扫码，1=现金，2=会员',
     remark        nvarchar(500)                          null comment '备注',
     create_by     varchar(64) default ''                null comment '创建者',
     create_time   timestamp   default CURRENT_TIMESTAMP not null comment '创建时间',
     update_by     varchar(64) default ''                null comment '更新者',
     update_time   timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
 ) comment '订单';
+
+drop table  if exists t_order_goods;
 create table  t_order_goods
 (
     order_detail_id      bigint                                not null comment 'id' primary key,
@@ -119,30 +125,26 @@ create table  t_order_goods
     goods_name bigint not null  comment '商品名称',
     price decimal(10,2) not null  comment  '单价',
     num  int  not null  comment  '数量',
-    total_amount_due decimal(10,2)  not null comment '应付总金额(total_discount_amount+total_amount)',
-    total_discount_amount decimal(10,2)  not null default 0.00 comment '折扣金额 ',
-    total_amount decimal(10,2)  not null comment '实际付款总金额（total_cash_amount+total_member_amount）',
-    total_cash_amount decimal(10,2)  not null comment '实际现金付款总金额',
-    total_member_amount decimal(10,2)  not null comment '实际会员付款总金额',
+    total_amount decimal(10,2)  not null comment '实际付款总金额 ',
     remark        nvarchar(500)                          null comment '备注',
     create_by     varchar(64) default ''                null comment '创建者',
     create_time   timestamp   default CURRENT_TIMESTAMP not null comment '创建时间',
     update_by     varchar(64) default ''                null comment '更新者',
     update_time   timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
 ) comment '购买商品';
+drop table  if exists t_order_member_deduct;
 create table  t_order_member_deduct
 (
      order_member_deduct_id      bigint                                not null comment 'id' primary key,
      order_id bigint not null  comment '订单id',
      member_id bigint not null  comment '会员id',
-     total_amount_due bigint not null  comment '应付金额',
-     discount_amount decimal(10,2)  not null default 0.00 comment '折扣金额',
      total_amount decimal(10,2)  not null comment '实际付款总金额',
      create_by     varchar(64) default ''                null comment '创建者',
      create_time   timestamp   default CURRENT_TIMESTAMP not null comment '创建时间',
      update_by     varchar(64) default ''                null comment '更新者',
      update_time   timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
 ) comment '会员付款记录';
+drop table  if exists t_order_recharge;
 create table  t_order_recharge
 (
     order_recharge_id      bigint                                not null comment 'id' primary key,
@@ -175,7 +177,7 @@ create table  t_order_desk_time
     update_time   timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
 )
     comment '订单计时';
-
+drop table  if exists t_order_tutor_time;
 create table  t_order_tutor_time
 (
     order_tutor_time_id      bigint                                not null comment '球桌编码'
@@ -193,7 +195,7 @@ create table  t_order_tutor_time
     update_by     varchar(64) default ''                null comment '更新者',
     update_time   timestamp   default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间'
 )
-    comment '订单计时';
+    comment '订单教练计时';
 
 
 
