b4ef006d6eb511409ffc00c61563cacf78da098f
eric <zeng3750@foxmail.com>
2024-09-14 00:38:24 +0800

    排队叫号弄好了

--- ruoyi-ui-cashier/src/views/cashier/desk/components/lineUp.vue
+++ ruoyi-ui-cashier/src/views/cashier/desk/components/lineUp.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="line-up-container">
-    <el-tabs v-model="currentTitle" class="line-up-tabs">
+    <el-tabs v-model="currentTitle">
       <el-tab-pane :label="place.label" name="basic" :key="'line_up'+place.value" :name="place.value"
-                   v-for="place in dict.type.store_desk_place">
-        <div class="num-container">
-          <div class="num-item" :key="place.value+'line_item_key'+item.num"
-               v-for="(item,index) in getLineUpList(place.value).numList">
-            <div class="num-item-text">
-              {{ item.num }}
+                   v-for="place in dict.type.store_desk_place"  style="height:90%;overflow-y:auto;overflow-x:hidden;">
+        <el-scrollbar class="right-scrollbar">
+          <div class="num-container">
+            <div class="num-item" :key="place.value+'line_item_key'+item.num"
+                 v-for="(item,index) in getLineUpList(place.value).numList">
+              <div class="num-item-text">
+                {{ item.num }}
+              </div>
+
+              <el-button class="num-item-btn" circle @click="onSpeechClick(item.num)"> 呼叫</el-button>
+              <el-button class="num-item-btn" type="success" circle @click="onRemoveNumClick(place.value,index)"> 用号
+              </el-button>
+              <el-button class="num-item-btn" type="danger" circle @click="onRemoveNumClick(place.value,index)"> 过号
+              </el-button>
+
             </div>
-
-            <el-button class="num-item-btn" circle @click="onSpeechClick(item.num)"> 呼叫</el-button>
-            <el-button class="num-item-btn" type="success" circle> 用号</el-button>
-            <el-button class="num-item-btn" type="danger" circle> 过号</el-button>
-
           </div>
-        </div>
+        </el-scrollbar>
       </el-tab-pane>
 
     </el-tabs>
@@ -58,10 +62,26 @@ export default {
         this.lineUpInfo = p.data || {}
       }).finally(this.closeLoading)
     },
+    onRemoveNumClick(type, idx) {
+      if (this.loading) {
+        return
+      }
+      this.loading = true;
+      let lineUp = JSON.parse(JSON.stringify(this.lineUpInfo));
+      let tempLineInfo = lineUp[type];
+      tempLineInfo.numList.splice(idx, 1);
+      if (tempLineInfo.numList.length === 0) {
+        tempLineInfo.currentNum = 0;
+      }
+      saveLineUp(lineUp).then(p => {
+        this.lineUpInfo = lineUp;
+      }).finally(this.closeLoading)
+    },
     onSpeechClick(num) {
-      callPCMethod(DeviceMethodNames.Speech, [  {content: num, emphasis: 1}, {content: "号客户请到前台"}])
+      callPCMethod(DeviceMethodNames.Speech, [{content: num, emphasis: 1}, {content: "号客户请到前台"}])
     },
     onCreateClick(type) {
+      this.currentTitle = type;
       if (this.loading) {
         return
       }
@@ -72,14 +92,14 @@ export default {
       let tempLineInfo = lineUp[type];
 
       let newNum = `${PreNum[parseInt(type)]}${tempLineInfo.currentNum + 1}`;
-      let newItem ={
+      let newItem = {
         num: newNum,
         createTime: parseTime(Date())
       };
       tempLineInfo.numList.push(newItem);
       tempLineInfo.currentNum += 1;
       saveLineUp(lineUp).then(p => {
-        this.lineUpInfo=lineUp;
+        this.lineUpInfo = lineUp;
       }).finally(this.closeLoading)
 
     },
