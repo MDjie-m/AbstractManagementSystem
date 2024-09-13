9c68b3ce32cbc8736985ff27141aa2d5cc627b5b
changhuainian <2824099816@qq.com>
2024-09-13 11:31:55 +0800

    sad

---
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/IJobChangeApplicationService.java
@@ -0,0 +1,61 @@
+package com.ruoyi.system.service;
+
+import java.util.List;
+import com.ruoyi.system.domain.JobChangeApplication;
+
+/**
+ * 调岗申请Service接口
+ * 
+ * @author KK
+ * @date 2024-09-13
+ */
+public interface IJobChangeApplicationService 
+{
+    /**
+     * 查询调岗申请
+     * 
+     * @param id 调岗申请主键
+     * @return 调岗申请
+     */
+    public JobChangeApplication selectJobChangeApplicationById(Long id);
+
+    /**
+     * 查询调岗申请列表
+     * 
+     * @param jobChangeApplication 调岗申请
+     * @return 调岗申请集合
+     */
+    public List<JobChangeApplication> selectJobChangeApplicationList(JobChangeApplication jobChangeApplication);
+
+    /**
+     * 新增调岗申请
+     * 
+     * @param jobChangeApplication 调岗申请
+     * @return 结果
+     */
+    public int insertJobChangeApplication(JobChangeApplication jobChangeApplication);
+
+    /**
+     * 修改调岗申请
+     * 
+     * @param jobChangeApplication 调岗申请
+     * @return 结果
+     */
+    public int updateJobChangeApplication(JobChangeApplication jobChangeApplication);
+
+    /**
+     * 批量删除调岗申请
+     * 
+     * @param ids 需要删除的调岗申请主键集合
+     * @return 结果
+     */
+    public int deleteJobChangeApplicationByIds(Long[] ids);
+
+    /**
+     * 删除调岗申请信息
+     * 
+     * @param id 调岗申请主键
+     * @return 结果
+     */
+    public int deleteJobChangeApplicationById(Long id);
+}
