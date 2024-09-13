89904a88d2947ad7dc616e9d85e10cb092d5ce29
changhuainian <2824099816@qq.com>
2024-09-13 16:40:46 +0800

    修改自动生成代码的实体

--- ruoyi-generator/src/main/java/com/ruoyi/generator/domain/GenTableColumn.java
+++ ruoyi-generator/src/main/java/com/ruoyi/generator/domain/GenTableColumn.java
@@ -1,19 +1,31 @@
 package com.ruoyi.generator.domain;
 
 import javax.validation.constraints.NotBlank;
+
+import com.baomidou.mybatisplus.annotation.FieldStrategy;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.ruoyi.common.core.domain.BaseEntity;
 import com.ruoyi.common.utils.StringUtils;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.ibatis.type.JdbcType;
 
 /**
  * 代码生成业务字段表 gen_table_column
- * 
+ *
  * @author ruoyi
  */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@TableName("gen_table_column")
 public class GenTableColumn extends BaseEntity
 {
     private static final long serialVersionUID = 1L;
 
     /** 编号 */
+    @TableId(value = "column_id")
     private Long columnId;
 
     /** 归属表编号 */
@@ -23,6 +35,7 @@ public class GenTableColumn extends BaseEntity
     private String columnName;
 
     /** 列描述 */
+    @TableField(updateStrategy = FieldStrategy.IGNORED, jdbcType = JdbcType.VARCHAR)
     private String columnComment;
 
     /** 列类型 */
@@ -36,24 +49,31 @@ public class GenTableColumn extends BaseEntity
     private String javaField;
 
     /** 是否主键（1是） */
+    @TableField(updateStrategy = FieldStrategy.IGNORED, jdbcType = JdbcType.VARCHAR)
     private String isPk;
 
     /** 是否自增（1是） */
+    @TableField(updateStrategy = FieldStrategy.IGNORED, jdbcType = JdbcType.VARCHAR)
     private String isIncrement;
 
     /** 是否必填（1是） */
+    @TableField(updateStrategy = FieldStrategy.IGNORED, jdbcType = JdbcType.VARCHAR)
     private String isRequired;
 
     /** 是否为插入字段（1是） */
+    @TableField(updateStrategy = FieldStrategy.IGNORED, jdbcType = JdbcType.VARCHAR)
     private String isInsert;
 
     /** 是否编辑字段（1是） */
+    @TableField(updateStrategy = FieldStrategy.IGNORED, jdbcType = JdbcType.VARCHAR)
     private String isEdit;
 
     /** 是否列表字段（1是） */
+    @TableField(updateStrategy = FieldStrategy.IGNORED, jdbcType = JdbcType.VARCHAR)
     private String isList;
 
     /** 是否查询字段（1是） */
+    @TableField(updateStrategy = FieldStrategy.IGNORED, jdbcType = JdbcType.VARCHAR)
     private String isQuery;
 
     /** 查询方式（EQ等于、NE不等于、GT大于、LT小于、LIKE模糊、BETWEEN范围） */
@@ -68,89 +88,9 @@ public class GenTableColumn extends BaseEntity
     /** 排序 */
     private Integer sort;
 
-    public void setColumnId(Long columnId)
-    {
-        this.columnId = columnId;
-    }
-
-    public Long getColumnId()
-    {
-        return columnId;
-    }
-
-    public void setTableId(Long tableId)
-    {
-        this.tableId = tableId;
-    }
-
-    public Long getTableId()
-    {
-        return tableId;
-    }
-
-    public void setColumnName(String columnName)
-    {
-        this.columnName = columnName;
-    }
-
-    public String getColumnName()
-    {
-        return columnName;
-    }
-
-    public void setColumnComment(String columnComment)
-    {
-        this.columnComment = columnComment;
-    }
-
-    public String getColumnComment()
-    {
-        return columnComment;
-    }
-
-    public void setColumnType(String columnType)
-    {
-        this.columnType = columnType;
-    }
-
-    public String getColumnType()
-    {
-        return columnType;
-    }
-
-    public void setJavaType(String javaType)
-    {
-        this.javaType = javaType;
-    }
-
-    public String getJavaType()
-    {
-        return javaType;
-    }
-
-    public void setJavaField(String javaField)
-    {
-        this.javaField = javaField;
-    }
-
-    public String getJavaField()
-    {
-        return javaField;
-    }
-
     public String getCapJavaField()
     {
         return StringUtils.capitalize(javaField);
-    }
-
-    public void setIsPk(String isPk)
-    {
-        this.isPk = isPk;
-    }
-
-    public String getIsPk()
-    {
-        return isPk;
     }
 
     public boolean isPk()
@@ -163,16 +103,6 @@ public class GenTableColumn extends BaseEntity
         return isPk != null && StringUtils.equals("1", isPk);
     }
 
-    public String getIsIncrement()
-    {
-        return isIncrement;
-    }
-
-    public void setIsIncrement(String isIncrement)
-    {
-        this.isIncrement = isIncrement;
-    }
-
     public boolean isIncrement()
     {
         return isIncrement(this.isIncrement);
@@ -181,16 +111,6 @@ public class GenTableColumn extends BaseEntity
     public boolean isIncrement(String isIncrement)
     {
         return isIncrement != null && StringUtils.equals("1", isIncrement);
-    }
-
-    public void setIsRequired(String isRequired)
-    {
-        this.isRequired = isRequired;
-    }
-
-    public String getIsRequired()
-    {
-        return isRequired;
     }
 
     public boolean isRequired()
@@ -203,16 +123,6 @@ public class GenTableColumn extends BaseEntity
         return isRequired != null && StringUtils.equals("1", isRequired);
     }
 
-    public void setIsInsert(String isInsert)
-    {
-        this.isInsert = isInsert;
-    }
-
-    public String getIsInsert()
-    {
-        return isInsert;
-    }
-
     public boolean isInsert()
     {
         return isInsert(this.isInsert);
@@ -221,16 +131,6 @@ public class GenTableColumn extends BaseEntity
     public boolean isInsert(String isInsert)
     {
         return isInsert != null && StringUtils.equals("1", isInsert);
-    }
-
-    public void setIsEdit(String isEdit)
-    {
-        this.isEdit = isEdit;
-    }
-
-    public String getIsEdit()
-    {
-        return isEdit;
     }
 
     public boolean isEdit()
@@ -243,16 +143,6 @@ public class GenTableColumn extends BaseEntity
         return isEdit != null && StringUtils.equals("1", isEdit);
     }
 
-    public void setIsList(String isList)
-    {
-        this.isList = isList;
-    }
-
-    public String getIsList()
-    {
-        return isList;
-    }
-
     public boolean isList()
     {
         return isList(this.isList);
@@ -261,16 +151,6 @@ public class GenTableColumn extends BaseEntity
     public boolean isList(String isList)
     {
         return isList != null && StringUtils.equals("1", isList);
-    }
-
-    public void setIsQuery(String isQuery)
-    {
-        this.isQuery = isQuery;
-    }
-
-    public String getIsQuery()
-    {
-        return isQuery;
     }
 
     public boolean isQuery()
@@ -283,46 +163,6 @@ public class GenTableColumn extends BaseEntity
         return isQuery != null && StringUtils.equals("1", isQuery);
     }
 
-    public void setQueryType(String queryType)
-    {
-        this.queryType = queryType;
-    }
-
-    public String getQueryType()
-    {
-        return queryType;
-    }
-
-    public String getHtmlType()
-    {
-        return htmlType;
-    }
-
-    public void setHtmlType(String htmlType)
-    {
-        this.htmlType = htmlType;
-    }
-
-    public void setDictType(String dictType)
-    {
-        this.dictType = dictType;
-    }
-
-    public String getDictType()
-    {
-        return dictType;
-    }
-
-    public void setSort(Integer sort)
-    {
-        this.sort = sort;
-    }
-
-    public Integer getSort()
-    {
-        return sort;
-    }
-
     public boolean isSuperColumn()
     {
         return isSuperColumn(this.javaField);
@@ -332,9 +172,9 @@ public class GenTableColumn extends BaseEntity
     {
         return StringUtils.equalsAnyIgnoreCase(javaField,
                 // BaseEntity
-                "createBy", "createTime", "updateBy", "updateTime", "remark",
+                "createBy", "createTime", "updateBy", "updateTime",
                 // TreeEntity
-                "parentName", "parentId", "orderNum", "ancestors");
+                "parentName", "parentId");
     }
 
     public boolean isUsableColumn()
