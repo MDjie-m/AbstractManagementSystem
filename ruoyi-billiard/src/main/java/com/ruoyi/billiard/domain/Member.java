05cf2382caec4860fa03a227d8b1540f55cd3862
eric <zeng3750@foxmail.com>
2024-09-24 01:14:59 +0800

    fix member bug

--- ruoyi-billiard/src/main/java/com/ruoyi/billiard/domain/Member.java
+++ ruoyi-billiard/src/main/java/com/ruoyi/billiard/domain/Member.java
@@ -12,7 +12,7 @@ import java.math.BigInteger;
 
 /**
  * 门店会员对象 t_member
- * 
+ *
  * @author zhoukeu
  * @date 2024-09-14
  */
@@ -22,34 +22,43 @@ import java.math.BigInteger;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Member extends MyBaseEntity
-{
+public class Member extends MyBaseEntity {
     private static final long serialVersionUID = 1L;
 
-    /** ID */
+    /**
+     * ID
+     */
 
     @TableId("member_id")
     private Long memberId;
 
-    /** 姓名 */
+    /**
+     * 姓名
+     */
     @Excel(name = "姓名")
 
     @TableField("real_name")
     private String realName;
 
-    /** 手机号 */
+    /**
+     * 手机号
+     */
     @Excel(name = "手机号")
 
     @TableField("mobile")
     private String mobile;
 
-    /** 当前金额 */
+    /**
+     * 当前金额
+     */
     @Excel(name = "当前金额")
 
     @TableField("current_amount")
     private BigDecimal currentAmount;
 
-    /** 历史总金额 */
+    /**
+     * 历史总金额
+     */
     @Excel(name = "历史总金额")
 
     @TableField("total_amount")
@@ -58,43 +67,60 @@ public class Member extends MyBaseEntity
     @TableField("pay_password")
     private String payPassword;
 
-    /** 性别（0=男，1=女，2=未知） */
+    /**
+     * 性别（0=男，1=女，2=未知）
+     */
     @Excel(name = "性别", readConverterExp = "0==男，1=女，2=未知")
 
     @TableField("sex")
     private String sex;
 
-    /** 门店 */
+    /**
+     * 门店
+     */
     @Excel(name = "门店")
 
     @TableField("store_id")
     private Long storeId;
 
-    /** 会员等级 */
+    /**
+     * 会员等级
+     */
     @Excel(name = "会员等级")
 
     @TableField("level_id")
     private BigInteger levelId;
 
-    /** 状态（0正常 1停用） */
+    @TableField(exist = false)
+    private String levelName;
+
+    /**
+     * 状态（0正常 1停用）
+     */
     @Excel(name = "状态", readConverterExp = "0=正常,1=停用")
 
     @TableField("status")
     private Integer status;
 
-    /** 删
-除标志（0代表存在 2代表删除） */
+    /**
+     * 删
+     * 除标志（0代表存在 2代表删除）
+     */
 
     @TableField("del_flag")
     private String delFlag;
 
-    /** 创建者Id */
+    /**
+     * 创建者Id
+     */
     @Excel(name = "创建者Id")
 
     @TableField("create_by_id")
     private Long createById;
 
-    /** 更新者Id */
+    /**
+     * 更新者Id
+     */
     @Excel(name = "更新者Id")
 
     @TableField("update_by_id")
@@ -104,18 +130,8 @@ public class Member extends MyBaseEntity
     private String storeName;
 
 
-
-
     @TableField(exist = false)
     private String keyword;
-
-
-
-
-
-
-
-
 
 
 }
