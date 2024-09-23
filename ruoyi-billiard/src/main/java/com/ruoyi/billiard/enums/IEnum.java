6f20495454451453d219b773714caafc0d32c460
eric <zeng3750@foxmail.com>
2024-09-23 14:03:30 +0800

    fix enum json serialize

--- ruoyi-billiard/src/main/java/com/ruoyi/billiard/enums/IEnum.java
+++ ruoyi-billiard/src/main/java/com/ruoyi/billiard/enums/IEnum.java
@@ -1,7 +1,10 @@
 package com.ruoyi.billiard.enums;
 
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public interface IEnum extends com.baomidou.mybatisplus.annotation.IEnum<Integer> {
+    @JsonValue
     Integer getValue();
     String getDesc();
 
