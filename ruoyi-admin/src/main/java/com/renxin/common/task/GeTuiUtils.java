58e105153c20c9e9f20d5c041a89aa0eb75668a7
zfsunj <zfsunj@isoftstone.com>
2024-09-26 16:42:54 +0800

    回调url放行

---
+++ ruoyi-admin/src/main/java/com/renxin/common/task/GeTuiUtils.java
@@ -0,0 +1,4 @@
+package com.renxin.common.task;
+
+public class GeTuiUtils {
+}
--- ruoyi-framework/src/main/java/com/renxin/framework/config/SecurityConfig.java
+++ ruoyi-framework/src/main/java/com/renxin/framework/config/SecurityConfig.java
@@ -122,7 +122,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                 .antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/*/api-docs", "/druid/**").permitAll()
                 //.antMatchers("/consultant/user/login","/consulted/user/wechatProgram/login","/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/*/api-docs", "/druid/**").permitAll()
                 //登录和回调, webSocket可匿名访问
-                .antMatchers("/**/login","/**/callback","/message").permitAll()
+                .antMatchers("/**/login","/**/callback","/**/wechatPayNotify","/message").permitAll()
                 // 对于登录login 注册register 验证码captchaImage 允许匿名访问
                 .antMatchers( "/register", "/captchaImage","/token/**","/**/sendSms","/**/loginBySmsCode").permitAll()
                 //.antMatchers("/system/**","/consultant/**").permitAll()
