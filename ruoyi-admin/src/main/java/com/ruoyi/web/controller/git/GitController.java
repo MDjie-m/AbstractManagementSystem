a6d63467f8e50adbc98c08895b546ce83ed6f9a9
郑小浩 <haoziu@163.com>
2024-10-01 17:53:31 +0800

    update: git推送接口更新

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/git/GitController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/git/GitController.java
@@ -1,14 +1,20 @@
 package com.ruoyi.web.controller.git;
 
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import com.ruoyi.common.annotation.Anonymous;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.utils.feishu.FeiShuService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * 通用请求处理
@@ -21,13 +27,25 @@ import javax.servlet.http.HttpServletRequest;
 public class GitController {
     private static final Logger log = LoggerFactory.getLogger(GitController.class);
 
+    @Autowired
+    FeiShuService feiShuService;
+
     /**
      * 通用上传请求（单个）
      */
     @RequestMapping("/pushStatus")
     public AjaxResult pushStatus(HttpServletRequest request, @RequestBody String postData) {
         AjaxResult ajax = AjaxResult.success();
-        ajax.put("ok", "yes");
+        Map<String, Object> gitMap = JSON.parseObject(postData, Map.class);
+        JSONObject projectObj = (JSONObject) gitMap.get("project");
+        String name = projectObj.getString("name");
+        String webUrl = projectObj.getString("web_url");
+        String userName = (String) gitMap.get("user_name");
+        JSONArray commits = (JSONArray) gitMap.get("commits");
+        JSONObject commitsObj = (JSONObject) commits.get(0);
+        String message = commitsObj.getString("message");
+
+        feiShuService.sendFeiShuGitMessage(webUrl, name, userName, message);
         return ajax;
     }
 }
