05406f193510359282f54c41b0b321c403563568
zfsunj <zfsunj@isoftstone.com>
2024-10-12 14:03:57 +0800

    /wechatProgram/updateUser    base64字符串格式的头像解析上传

--- ruoyi-paychology/src/main/java/com/renxin/psychology/service/impl/PsyUserServiceImpl.java
+++ ruoyi-paychology/src/main/java/com/renxin/psychology/service/impl/PsyUserServiceImpl.java
@@ -2,12 +2,16 @@ package com.renxin.psychology.service.impl;
 
 import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSONObject;
+import com.qcloud.cos.model.UploadResult;
 import com.renxin.common.constant.IntegralRecordConstants;
+import com.renxin.common.core.domain.AjaxResult;
 import com.renxin.common.core.domain.dto.LoginDTO;
 import com.renxin.common.core.domain.vo.LoginVO;
 import com.renxin.common.event.publish.IntegralPublisher;
 import com.renxin.common.exception.ServiceException;
 import com.renxin.common.utils.DateUtils;
+import com.renxin.common.utils.IDhelper;
+import com.renxin.common.utils.cos.COSClientFactory;
 import com.renxin.psychology.domain.PsyConsultantSchedule;
 import com.renxin.psychology.domain.PsyUser;
 import com.renxin.psychology.dto.OrderItemDTO;
@@ -19,14 +23,17 @@ import com.renxin.psychology.service.IPsyUserService;
 import com.renxin.user.domain.PsyUserIntegralRecord;
 import com.renxin.user.service.IPsyUserIntegralRecordService;
 import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * 用户Service业务层处理
@@ -47,7 +54,7 @@ public class PsyUserServiceImpl implements IPsyUserService {
 
     @Resource
     private IPsyConsultantScheduleService scheduleService;
-    
+
     @Resource
     private IPsyConsultOrderItemService orderItemService;
 
@@ -108,10 +115,50 @@ public class PsyUserServiceImpl implements IPsyUserService {
     public int updatePsyUser(PsyUser psyUser) {
         //判断头像信息的格式, 若为base64则需解析图片后上传获取url
         String avatar = psyUser.getAvatar();
-        if (!avatar.contains("http")){
-            
+        if (ObjectUtils.isNotEmpty(avatar) && !avatar.contains("http")) {
+            String module = "zx";
+            String type = "avatar";
+            UploadResult upload = null;
+            InputStream inputStream = null;
+            try {
+                MultipartFile file = base64ToMultipartFile(avatar);
+                String fileName = StrUtil.format("{}_{}_{}", type, IDhelper.getNextId(), new Random().nextInt(999999) + "用户头像.jpg");
+                //  调用文件服务器方法，实现文件上传改写
+                inputStream = file.getInputStream();
+                upload = COSClientFactory.upload(inputStream, fileName, module);
+                String key = upload.getKey();
+                String url = COSClientFactory.getObjUrl(key, module);
+                AjaxResult ajax = AjaxResult.success();
+                psyUser.setAvatar(url);
+            } catch (Exception e){
+                throw new ServiceException("base64转换图片上传异常");
+            }
         }
         return psyUserMapper.updatePsyUser(psyUser);
+    }
+
+    //将base64字符串  转换为MultipartFile对象
+    private MultipartFile base64ToMultipartFile(String base64) {
+        // 分离base64头部信息
+        //String[] baseStrs = base64.split(",");
+
+        // 解码Base64字符串
+        byte[] decodedBytes = Base64.getDecoder().decode(base64);
+
+        // 创建MultipartFile对象
+        MultipartFile multipartFile = null;
+        try {
+            // 转换为InputStream
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(decodedBytes);
+
+            // 使用MockMultipartFile来创建一个新的MultipartFile对象
+            multipartFile = new MockMultipartFile("file", byteArrayInputStream);
+
+        } catch (Exception e) {
+            throw new ServiceException("base64转换图片异常");
+        }
+
+        return multipartFile;
     }
 
     /**
@@ -184,10 +231,9 @@ public class PsyUserServiceImpl implements IPsyUserService {
         PsyUser phoneUser = psyUserMapper.queryUserByAccount(loginDTO.getPhone());
         //若该手机号已经存在用户，直接将微信信息更新至该用户,并删除此微信用户
         if (phoneUser != null) {
-            throw new ServiceException("此手机号已绑定其他用户");
-            /*psyUserMapper.updatePsyUser(PsyUser.builder().id(phoneUser.getId()).wxOpenid(user.getWxOpenid()).avatar(user.getAvatar()).name(user.getName()).build());
+            psyUserMapper.updatePsyUser(PsyUser.builder().id(phoneUser.getId()).wxOpenid(user.getWxOpenid()).avatar(user.getAvatar()).name(user.getName()).build());
             psyUserMapper.deletePsyUserById(user.getId());
-            loginDTO.setUserId(phoneUser.getId());*/
+            loginDTO.setUserId(phoneUser.getId());
         } else {
             psyUserMapper.updatePsyUser(PsyUser.builder().id(user.getId()).phone(loginDTO.getPhone()).build());
             loginDTO.setUserId(user.getId());
@@ -231,7 +277,7 @@ public class PsyUserServiceImpl implements IPsyUserService {
                     }
                 }
             }*/
-            psyUser.setOrderItemList(orderItemChargeList);  
+            psyUser.setOrderItemList(orderItemChargeList);
         }
         return psyUser;
     }
