81dd3803fa396cd0191e239a0c0c9aacf47b162e
zhoukeu <zhoukeu@163.com>
2024-10-24 23:20:32 +0800

    zhoukeu9.27 小程序统计报表订单编号fix

--- ruoyi-billiard/src/main/java/com/ruoyi/billiard/enums/OrderType.java
+++ ruoyi-billiard/src/main/java/com/ruoyi/billiard/enums/OrderType.java
@@ -9,7 +9,7 @@ import java.util.*;
 @Getter
 @AllArgsConstructor
 public enum OrderType implements IEnum {
-    AGGREGATE_CONSUMPTION(0, "总消费",null),
+    AGGREGATE_CONSUMPTION(0, "总营业额",null),
     TABLE_CHARGE(1, "台桌费用","D"),
     MEMBER_RECHARGE(2, "会员充值","R"),
     COMMODITY_PURCHASE(3, "商品购买","B"),
