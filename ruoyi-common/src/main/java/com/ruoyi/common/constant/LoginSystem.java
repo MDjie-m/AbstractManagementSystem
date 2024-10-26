f05b43693813258d3e081664dc3831422e8871dd
eric <zeng3750@foxmail.com>
2024-10-26 21:41:16 +0800

    fix

--- ruoyi-common/src/main/java/com/ruoyi/common/constant/LoginSystem.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/constant/LoginSystem.java
@@ -6,9 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum LoginSystem {
-    BACKEND_SYSTEM(1,"后台系统"),
-    CASHIER_SYSTEM(2,"收银系统"),
-    MINI_APP_SYSTEM(3,"小程序"),
+    BACKEND_SYSTEM(0,"后台系统"),
+    CASHIER_SYSTEM(1,"收银系统"),
+    MINI_APP_SYSTEM(2,"小程序"),
     ;
     private final int value;
     private final String desc;
