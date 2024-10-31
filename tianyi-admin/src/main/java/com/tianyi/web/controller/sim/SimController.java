51a86af1b0986c32927f0dc843f0fb1c9e9378b7
zhujun_xiaoxin <zhujun_xiaoxin@noreply.gitcode.com>
2024-10-31 17:49:45 +0800

    SIM卡控制器接口完善

--- tianyi-admin/src/main/java/com/tianyi/web/controller/sim/SimController.java
+++ tianyi-admin/src/main/java/com/tianyi/web/controller/sim/SimController.java
@@ -3,17 +3,48 @@ package com.tianyi.web.controller.sim;
 import com.tianyi.common.core.domain.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "SIM卡管理", value = "SIM卡管理模块", description = "SIM卡管理模块描述")
+@Api(tags = "SIM卡管理", description = "SIM卡管理模块描述")
 @RestController
 @RequestMapping("/sim")
 public class SimController {
 
-    @ApiOperation(tags = "SIM卡停机", value = "sim卡停机分析")
-    public R<String> stopAnalysis(){
-        return R.ok("停止分析");
+    @ApiOperation(value = "sim卡停机清单查询")
+    @GetMapping("/shutdownQuery")
+    public R<String> shutdownQuery() {
+        return R.ok("停机查询");
     }
 
+    @ApiOperation(value = "sim卡断网清单查询")
+    @GetMapping("/disconQuery")
+    public R<String> disconQuery() {
+        return R.ok("断网查询");
+    }
+
+    @ApiOperation(value = "sim卡NB异常清单查询")
+    @GetMapping("/NBAbnormalQuery")
+    public R<String> NBAbnormalQuery() {
+        return R.ok("NB异常查询");
+    }
+
+    @ApiOperation(value = "套餐到期预警")
+    @GetMapping("/packExpWarning")
+    public R<String> packExpWarning() {
+        return R.ok("套餐到期预警");
+    }
+
+    @ApiOperation(value = "长期未使用预警")
+    @GetMapping("/longTermNoUseWarning")
+    public R<String> longTermNoUseWarning() {
+        return R.ok("长期未使用预警");
+    }
+
+    @ApiOperation(value = "套餐欠费预警")
+    @GetMapping("/upPaidWarning")
+    public R<String> upPaidWarning() {
+        return R.ok("套餐欠费预警");
+    }
 }
