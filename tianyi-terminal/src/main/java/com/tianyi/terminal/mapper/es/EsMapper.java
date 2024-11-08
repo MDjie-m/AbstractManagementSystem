00ad1f7adbbca48b21dd53f59b5243dff92f1366
zhujun_xiaoxin <zhujun_xiaoxin@noreply.gitcode.com>
2024-11-08 17:04:13 +0800

    es框架修改

--- tianyi-terminal/src/main/java/com/tianyi/terminal/mapper/es/EsMapper.java
+++ tianyi-terminal/src/main/java/com/tianyi/terminal/mapper/es/EsMapper.java
@@ -3,6 +3,6 @@ package com.tianyi.terminal.mapper.es;
 
 import org.dromara.easyes.core.kernel.BaseEsMapper;
 
-public interface EsMapper extends BaseEsMapper {
+public interface EsMapper extends BaseEsMapper<Object> {
 
 }
