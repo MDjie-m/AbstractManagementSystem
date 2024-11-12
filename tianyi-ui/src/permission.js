14412bc7682e5ca3bc24f0510cedc38a0c1add9d
hanyu <1325216468@qq.com>
2024-11-12 14:53:04 +0800

    删除修改前端的错误代码

--- tianyi-ui/src/permission.js
+++ tianyi-ui/src/permission.js
@@ -12,8 +12,6 @@ const whiteList = ['/login', '/register']
 
 router.beforeEach((to, from, next) => {
   NProgress.start()
-  next()
-  return
   if (getToken()) {
     to.meta.title && store.dispatch('settings/setTitle', to.meta.title)
     /* has token*/
